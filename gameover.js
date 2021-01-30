dab00d5eadadcd676dadfa59bb10f5b110693282
Normantas <kudzma.normantas@gmail.com>
2021-01-30 18:28:11 +0200

    Stuff

--- gameover.js
+++ gameover.js
@@ -1,11 +1,14 @@
 const gameover = {
 	img: void 0,
+	gif: void 0,
 	init: () => {
-		gameover.img = makeImage("nor_asset/gameover.png", 128, 128);
+		gameover.gif = makeImage("nor_asset/gameover.gif", 128, 128);
+		gameover.img = makeImage("nor_asset/gameover.png", 110, 310);
 	},
 	draw: () => {
 		background(255);
 		gameover.img.draw();
+		gameover.gif.draw();
 	},
 	clicked: (cx, cy) => {
 		level_nr = 1;
