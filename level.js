595255fcc2fcc85e160467f0c6decf8a9bf0d0a9
Darvydas <darvydelis@gmail.com>
2021-01-30 11:19:39 +0200

    Implemented initial level generation

--- level.js
+++ level.js
@@ -1,11 +1,65 @@
+const TILE_ALIVE = 1;
+const TILE_DEAD = 0;
+const dimensions = 10;
+const number_of_rooms = 20;
+
+function TileInDungeon(level, x, y) {
+	if (x < 0 || x >= dimensions || y < 0 || y >= dimensions) {
+		return false;
+	}
+	return true;
+}
+
+function CanPlaceRoom(level, x, y) {
+	if (TileInDungeon(level, x - 1, y - 1)) {
+		if (level[x - 1][y - 1] == TILE_ALIVE) {
+			return true;
+		}
+	}
+	if (TileInDungeon(level, x + 1, y - 1)) {
+		if (level[x + 1][y - 1] == TILE_ALIVE) {
+			return true;
+		}
+	}
+	if (TileInDungeon(level, x - 1, y + 1)) {
+        if (level[x - 1][y + 1] == TILE_ALIVE) {
+			return true;
+		}
+	}
+	if (TileInDungeon(level, x + 1, y + 1)) {
+        if (level[x + 1][y + 1] == TILE_ALIVE) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function ShouldPlace() {
+	return Math.floor((Math.random() * 100) + 1) <= 33;
+}
+
 function getLevel() {
-	let level = [
-		[ 1, 1, 1, 0, 1 ],
-		[ 0, 0, 1, 1, 1 ],
-		[ 0, 1, 1, 0, 1 ],
-		[ 1, 1, 0, 1, 1 ],
-		[ 0, 1, 1, 0, 0 ],
-	];
+	let level = Array(dimensions).fill().map(() => Array(dimensions).fill(0));
+	level[0][0] = TILE_ALIVE;
+	let rooms_created = 0;
+	while (rooms_created < number_of_rooms) {
+		var tile_created = false;
+		for (var x = 0; x < dimensions; x++) {
+			for (var y = 0; y < dimensions; y++) {
+				if (CanPlaceRoom(level, x, y) && ShouldPlace()) {
+					level[x][y] = TILE_ALIVE;
+					rooms_created++;
+                    tile_created = true;
+				}
+				if (tile_created) {
+					break;
+				}
+			}
+			if (tile_created) {
+				break;
+			}
+		}
+	}
 	return level;
 }
 
